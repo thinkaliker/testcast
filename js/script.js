6cda115865593a9de507cd53fadbdf7737ae0c30
Adam Chao <adam@thinkaliker.com>
2015-07-11 22:12:13 -0700

    disable image

--- js/script.js
+++ js/script.js
@@ -80,14 +80,13 @@ function loadMedia() {
     return;
   }
 
-  var mediaInfo = new
-  chrome.cast.media.MediaInfo('http://i.imgur.com/mOnvOJG.png');
-  mediaInfo.contentType = 'image/png';
+  //var mediaInfo = new chrome.cast.media.MediaInfo('http://i.imgur.com/mOnvOJG.png');
+  //mediaInfo.contentType = 'image/png';
 
   var request = new chrome.cast.media.LoadRequest(mediaInfo);
   request.autoplay = true;
 
-  session.loadMedia(request, onLoadSuccess, onLoadError);
+  //session.loadMedia(request, onLoadSuccess, onLoadError);
 }
 
 function onLoadSuccess() {
