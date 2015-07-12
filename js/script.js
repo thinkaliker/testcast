73da021792afa9948ad2075ec3ed3aca52253570
Adam Chao <adam@thinkaliker.com>
2015-07-11 20:34:44 -0700

    connection

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//http://blog.matthewdfuller.com/2014/05/chromecast-development-tutorial-your.html
+
+var session = null;
+
 $(document).ready(function() {
   var loadCastInterval = setInterval(function() {
     if (chrome.cast.isAvailable) {
@@ -9,3 +13,54 @@ $(document).ready(function() {
     }
   }, 1000);
 });
+
+function initializeCastApi() {
+  var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+  var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+  var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+    sessionListener,
+    receiverListener);
+  chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
+};
+
+function sessionListener(e) {
+  session = e;
+  console.log('New session');
+  if (session.media.length != 0) {
+    console.log('Found ' + session.media.length + ' sessions.');
+  }
+}
+
+function receiverListener(e) {
+  if (e === 'available') {
+    console.log("Chromecast was found on the network.");
+  } else {
+    console.log("There are no Chromecasts available.");
+  }
+}
+
+function onInitSuccess() {
+  console.log("Initialization succeeded");
+}
+
+function onInitError() {
+  console.log("Initialization failed");
+}
+
+$('#castme').click(function() {
+  launchApp();
+});
+
+function launchApp() {
+  console.log("Launching the Chromecast App...");
+  chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+}
+
+function onRequestSessionSuccess(e) {
+  console.log("Successfully created session: " + e.sessionId);
+  session = e;
+}
+
+function onLaunchError() {
+  console.log("Error connecting to the Chromecast.");
+}
